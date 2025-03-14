22d1aa6115017b5828be4abc6702c62b71e861dc
MIGUEL_ <Miguelortence@gmail.com>
2025-03-14 15:51:00 -0400

    Add style ao projeto

---
+++ script.js
@@ -0,0 +1,8 @@
+// Função para mostrar ofertas ao clicar no botão
+function mostrarOfertas() {
+alert("Confira nossas ofertas especiais! Descontos de até 50% em alguns produtos!");
+}
+
+// Função para adicionar produtos ao carrinho
+function adicionarAoCarrinho(produto) {
+alert(produto + " foi adicionado ao carrinho!");
